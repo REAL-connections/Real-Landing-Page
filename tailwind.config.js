98290de06cded766d75e17cc972f57c0042a829e
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-07 21:17:44 -0600

    starter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,48 @@
-const { fontFamily } = require(`tailwindcss/defaultTheme`)
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
     extend: {
-      fontFamily: {
-        sans: ['Fira Sans', ...fontFamily.sans],
-      },
-      screens: {
-        xs: '420px',
-        sm: '576px',
-        md: '768px',
-        lg: '992px',
-        xl: '1280px',
-        '2xl': '1440px',
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              color: theme('colors.current'),
-            },
-          },
+      colors: {
+        primary: {
+          100: '#E6F6FE',
+          200: '#C0EAFC',
+          300: '#9ADDFB',
+          400: '#4FC3F7',
+          500: '#03A9F4',
+          600: '#0398DC',
+          700: '#026592',
+          800: '#014C6E',
+          900: '#013349',
         },
-      }),
-    },
-    colors: {
-      inherit: 'inherit',
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000000',
-      white: '#ffffff',
-      gray: colors.slate,
-      primary: colors.indigo,
-      secondary: colors.rose,
-      tertiary: colors.emerald,
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+      },
+      lineHeight: {
+        hero: '4.5rem',
+      },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-  variants: {
-    extend: {},
-  },
-}
+  variants: {},
+  plugins: [],
+};
