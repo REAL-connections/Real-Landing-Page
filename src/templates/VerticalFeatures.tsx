01220c301bcfb4764e8f7b276d76e03b9543b065
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-08 11:09:45 -0600

    chore: update feature list wording

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -7,8 +7,8 @@ const VerticalFeatures = () => (
     description="Online dating without the FAKE. Real people. Real matches. Real dating."
   >
     <VerticalFeatureRow
-      title="No catfishes/bot accounts"
-      description="Did you know 1/4 accounts on Tinder are fake? Using unique features and algorithms, we ensure that there are no catfishes, and no bots. Every profile is a real person."
+      title="No catfishes or bot accounts"
+      description="Did you know 1/4 accounts on Tinder are fake? We have found a way ensure that there are no catfishes, and no bots. Every profile is a real person."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
@@ -21,7 +21,7 @@ const VerticalFeatures = () => (
     />
     <VerticalFeatureRow
       title="Matching to message"
-      description="Lastly, REAL dating will inherently promote users prioritzing messaging active matches over getting new matches. This will ensure that you get the best possibility of a real connection."
+      description="Lastly, REAL dating will naturally promote users prioritzing messaging active matches over getting new matches. This will ensure that you get the best possibility of a real connection."
       image="/assets/images/feature3.svg"
       imageAlt="Third feature alt text"
     />
