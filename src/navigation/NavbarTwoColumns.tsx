826f271b10e508ed63e3c6dd0e439a54a27e69ac
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-07 21:38:06 -0600

    feat: updated hero

--- src/navigation/NavbarTwoColumns.tsx
+++ src/navigation/NavbarTwoColumns.tsx
@@ -1,10 +1,9 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-import Link from 'next/link';
+import Link from "next/link";
 
 type INavbarProps = {
   logo: ReactNode;
-  children: ReactNode;
 };
 
 const NavbarTwoColumns = (props: INavbarProps) => (
@@ -14,13 +13,6 @@ const NavbarTwoColumns = (props: INavbarProps) => (
         <a>{props.logo}</a>
       </Link>
     </div>
-
-    <nav>
-      <ul className="navbar flex items-center font-medium text-xl text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
-
     <style jsx>
       {`
         .navbar :global(li:not(:first-child)) {
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,44 +1,45 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { Background } from '../background/Background';
-import { Button } from '../button/Button';
-import { HeroOneButton } from '../hero/HeroOneButton';
-import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import { Logo } from './Logo';
+import { Background } from "../background/Background";
+import { Button } from "../button/Button";
+import { HeroOneButton } from "../hero/HeroOneButton";
+import { Section } from "../layout/Section";
+import { NavbarTwoColumns } from "../navigation/NavbarTwoColumns";
+import { Logo } from "./Logo";
 
 const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Sign in</a>
-          </Link>
-        </li>
-      </NavbarTwoColumns>
+      <NavbarTwoColumns logo={<Logo xl />} />
     </Section>
 
     <Section yPadding="pt-20 pb-32">
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {"REAL Dating\n"}
+            <span className="text-primary-500">
+              Real people. Real matches. Real dating.{" "}
+            </span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="Subscribe to our newsletter and get the latest updates about our release in late 2022."
         button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
+          <form>
+            <input
+              type="text"
+              placeholder="Email"
+              className="p-4 w-1/2 border border-gray-300 rounded-lg"
+            />
             <a>
-              <Button xl>Download Your Free Theme</Button>
+              <button
+                type="submit"
+                className="bg-blue-500 text-white p-4 rounded-lg"
+              >
+                Submit
+              </button>
             </a>
-          </Link>
+          </form>
         }
       />
     </Section>
