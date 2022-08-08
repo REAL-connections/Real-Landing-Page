3d12f822572f491b1d1b80c1fdedfd2fa5fee0e3
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-07 23:17:53 -0600

    Feat: feature list descriptions

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -8,20 +8,20 @@ const VerticalFeatures = () => (
   >
     <VerticalFeatureRow
       title="No catfishes/bot accounts"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      description="Did you know 1/4 accounts on Tinder are fake? Using unique features and algorithms, we ensure that there are no catfishes, and no bots. Every profile is a real person."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
       title="Active accounts"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      description="Additionally, using our unqiue features and algorithms, we ensure that every account is active. Active accounts means more real people in your area actively looking for a real connection."
       image="/assets/images/feature2.svg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
       title="Matching to message"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      description="Lastly, REAL dating will inherently promote users prioritzing messaging active matches over getting new matches. This will ensure that you get the best possibility of a real connection."
       image="/assets/images/feature3.svg"
       imageAlt="Third feature alt text"
     />
