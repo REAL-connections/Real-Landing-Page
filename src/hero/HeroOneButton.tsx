4597dcb2fa06702bef67ce185b5beae2824ff30b
Jacob Wright <jacob.wright.ut@gmail.com>
2022-08-21 13:11:08 -0600

    FIX: HERO title and subtitle

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -14,8 +14,8 @@ const HeroOneButton = (props: IHeroOneButtonProps) => (
     </h1>
     <div className="flex flex-col mt-10 align-middle md:flex-row justify-between">
       <div>
-        <h2 className="text-[#F9BC60] text-5xl">
-          Real people looking for genuine relationships.
+        <h2 className="text-[#F9BC60] text-4xl">
+          A focus on genuinity and authenticity in online dating.
         </h2>
         <div className="text-2xl mt-4 mb-16">{props.description}</div>
 
